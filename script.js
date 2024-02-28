8d5ec7b60c2c842f8cca7d7dc7468ea6564eaa43
droopyantidote <144815313+droopyantidote@users.noreply.github.com>
2024-02-28 16:10:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+  const circles = document.querySelectorAll('.circle');
+  const prevButton = document.getElementById('prev');
+  const nextButton = document.getElementById('next');
+  let currentIndex = 0;
+
+  nextButton.addEventListener('click', () => {
+    if (currentIndex < circles.length - 1) {
+      currentIndex++;
+      circles[currentIndex].classList.add('active');
+      prevButton.disabled = false;
+    }
+
+    if (currentIndex === circles.length - 1) {
+      nextButton.disabled = true;
+    }
+  });
+
+  prevButton.addEventListener('click', () => {
+    if (currentIndex > 0) {
+      circles[currentIndex].classList.remove('active');
+      currentIndex--;
+      nextButton.disabled = false;
+    }
+
+    if (currentIndex === 0) {
+      prevButton.disabled = true;
+    }
+  });
